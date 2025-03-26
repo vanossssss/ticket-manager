130fa1cfdd1d10ea4b2a198ec8917d7c46196c87
vanossssss <kurdukov.434@mail.ru>
2025-03-26 15:42:50 +0500

    initial commit

---
+++ src/test/java/com/project/ticket_manager/TicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ticket_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
